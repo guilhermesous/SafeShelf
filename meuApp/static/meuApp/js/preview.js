7e502177629787b9e6555935edf3e3c8d22aabc1
Guilherme <guilherme.sousathe@gmail.com>
2024-02-24 20:55:24 -0300

    Ajustes

---
+++ meuApp/static/meuApp/js/preview.js
@@ -0,0 +1,28 @@
+const inputFile = document.querySelector("#id_imagem");
+const pictureImage = document.querySelector(".picture__image");
+const pictureImageTxt = "Escolha uma Imagem <img class='acoes' src='../../static/meuApp/imagens/icons8-imagem-100.png' alt=''>";
+pictureImage.innerHTML = pictureImageTxt;
+
+inputFile.addEventListener("change", function (e) {
+  const inputTarget = e.target;
+  const file = inputTarget.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function (e) {
+      const readerTarget = e.target;
+
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.classList.add("picture__img");
+
+      pictureImage.innerHTML = "";
+      pictureImage.appendChild(img);
+    });
+
+    reader.readAsDataURL(file);
+  } else {
+    pictureImage.innerHTML = pictureImageTxt;
+  }
+});
